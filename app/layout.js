fddfca3981223b89e2cf5b8bab4b8af3480dd16d
siyamregn77 <yeshidagnasiyamregn@gmail.com>
2025-08-06 11:23:17 +0300

    import componnet into page and theme inside layout

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,5 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { ThemeProvider } from "@/components/theme-provider";
 
 export const metadata = {
   title: "Create Next App",
@@ -18,11 +8,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="en" suppressHydrationWarning>
+      <body className="overflow-auto scrollbar-hide">
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem={true}>
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- app/page.js
+++ app/page.js
@@ -1,9 +1,19 @@
-import Image from "next/image";
+import Navbar from "../components/Navbar.js";
+import Hero from "../components/Hero.js";
+import Footer from "../components/Footer.js";
+import FQA from "../components/FQA.js";
+import Accordion from "@/components/Accordion.js";
+import FaqLast from "@/components/FaqLast.js";
 
 export default function Home() {
   return (
-    <div>
-      Hello world
+    <div className="overflow-hidden dark:bg-black dark:text-white">
+      <Navbar />
+      <Hero />
+      <Accordion />
+      <FQA />
+      <FaqLast />
+      <Footer />
     </div>
   );
 }
